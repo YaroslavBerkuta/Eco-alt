4d192412d5a2f7aa85f6e485683fedebc5634214
YaroslavBerkuta <yaroslavberkuta@gmail.com>
2022-01-17 18:06:51 +0300

    acordion shop

--- src/js/main.js
+++ src/js/main.js
@@ -80,3 +80,18 @@ const filterTovar = new Swiper(".filter__tovar", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
