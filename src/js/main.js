8bea26c3068dde4643f6827a1fcb01792c755fc8
YaroslavBerkuta <yaroslavberkuta@gmail.com>
2022-01-17 14:14:02 +0300

    main page catalog

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import * as flsFunctions from "./modules/function.js";
-
+import Swiper, { Pagination } from "swiper";
 flsFunctions.isWebp();
-
+Swiper.use([Pagination]);
 const menuList = document.querySelectorAll(".menu > li");
 const submenu = document.querySelectorAll(".sub-menu");
 menuList.forEach((item) => {
@@ -16,4 +16,44 @@ menuList.forEach((item) => {
       });
     }
   });
+});
+
+const brandSwiper = new Swiper(".brand-swiper", {
+  wrapperClass: "brand-wrapper",
+  slideClass: "brand-item",
+  spaceBetween: 24,
+  slidesPerView: 6,
+  loop: true,
+});
+
+const filterCatalog = new Swiper(".filter__catalog", {
+  wrapperClass: "filter__flex",
+  slideClass: "filter__item",
+  spaceBetween: 24,
+  loop: true,
+  slidesPerView: 6,
+  centeredSlides: true,
+});
+
+const filterItem = document.querySelectorAll(".filter__item");
+
+filterItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    filterItem.forEach((filter) => {
+      filter.classList.remove("active");
+    });
+    item.classList.add("active");
+  });
+});
+
+const filterTovar = new Swiper(".filter__tovar", {
+  slideClass: "filter___cart",
+  slidesPerView: 8,
+  grid: {
+    rows: 2,
+  },
+  pagination: {
+    el: ".swiper-pagination.filter__tovar-pagination",
+    clickable: true,
+  },
 });
